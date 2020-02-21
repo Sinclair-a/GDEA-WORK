a72ecb954b3825d213dfbfa856f1b2ebce1bdec7
code007-700 <61311609+code007-700@users.noreply.github.com>
2020-02-21 17:40:23 +0800

    vue.js

---
+++ 000WEB/00-Apache-Projiect/main.js
@@ -0,0 +1,23 @@
+// var src = "200";
+// src = prompt("密码？", "")
+// if (src == "123") {
+//     document.write("欢迎光临");
+// } else {
+//     document.write("密码错误");
+// }
+// document.write("<br>");
+// alert(parseInt(src) + 321);
+
+function ToGreen() {
+    div2.style.width = '300px';
+    div2.style.height = '300px';
+    div2.style.background= "green";
+}
+function ToRed() {
+    div2.style.width = '50px';
+    div2.style.height = '50px';
+    div2.style.background= "red";
+}   
+
+//<p id="aa"></p>
+// document.getElementById("aa").innerHTML=12;
